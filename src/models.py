09f5ce1858b60466492cfd95ef1d962a3f4f81bb
ZeqeB <85196913+ezebellino@users.noreply.github.com>
2024-10-20 00:37:19 +0000

    realizando cambios

--- src/models.py
+++ src/models.py
@@ -1,32 +1,63 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
+    
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50), nullable=False)  
+    address = Column(String(50), nullable=False) 
+    phone = Column(String(50), nullable=False) 
+    date = Column(Date(), unique=True, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    favs = relationship("Fav", back_populates="user")
+    characters = relationship("Character", back_populates="user")
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50), nullable=False, unique=True)  
+    description = Column(String(200), nullable=True)
 
-    def to_dict(self):
-        return {}
+    favs = relationship("Fav", back_populates="planet")
+    characters = relationship("Character", back_populates="planet")
+     
 
-## Draw from SQLAlchemy base
+class Character(Base):
+    __tablename__ = 'character'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False, unique=True)  
+    description = Column(String(200), nullable=True) 
+
+    favs = relationship("Fav", back_populates="character")
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship("User", back_populates="characters")
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship("Planet", back_populates="characters")
+
+
+class Fav(Base):
+    __tablename__ = 'fav'
+    
+    id_user = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    id_character = Column(Integer, ForeignKey('character.id'), primary_key=True)
+    id_planet = Column(Integer, ForeignKey('planet.id'), primary_key=True)
+
+    user = relationship("User", back_populates="favs")
+    character = relationship("Character", back_populates="favs")
+    planet = relationship("Planet", back_populates="favs")
+
 render_er(Base, 'diagram.png')
